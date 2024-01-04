9c33acee3aa532ee5e6e397a182166589fde7edf
prunus202401 <prunus202401@gmail.com>
2024-01-05 04:25:04 +0900

    カリキュラム解答16

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,7 +8,6 @@ use App\Models\Post;
 
 class PostController extends Controller
 {
-    
     /**
     * Post一覧を表示する
     * 
@@ -17,7 +16,14 @@ class PostController extends Controller
     */
     public function index(Post $post)//インポートしたPostをインスタンス化して$postとして使用。
     {
-        return $post->get();//$postの中身を戻り値にする。
+        // return $post->get();//$postの中身を戻り値にする。
+        
+        // return view('posts.index')->with(['posts' => $post->get()]);  
+       //blade内で使う変数'posts'と設定。'posts'の中身にgetを使い、インスタンス化した$postを代入。
+       
+        // return view('posts.index')->with(['posts' => $post->getByLimit()]);
+        
+        //getPaginateByLimit()はPost.phpで定義したメソッドです。
+        return view('posts.index')->with(['posts' => $post->getPaginateByLimit()]);
     }
-    
 }
